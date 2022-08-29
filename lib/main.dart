f34b6dcc3e2f5e17e5b4fce2e5c282d794f68f51
ducanh1699 <ducanh1699@gmail.com>
2022-08-30 00:38:27 +0700

    ducanh add icon, text, images

--- lib/main.dart
+++ lib/main.dart
@@ -8,39 +8,39 @@ void main() {
   runApp(const MyApp());
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
+// class RandomWords extends StatefulWidget {
+//   const RandomWords({super.key});
 
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
+//   @override
+//   State<RandomWords> createState() => _RandomWordsState();
+// }
 
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    final _suggestions = <WordPair>[];
-    final _biggerFont = const TextStyle(fontSize: 18);
-    // return Text(wordPair.asPascalCase);
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
+// class _RandomWordsState extends State<RandomWords> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final wordPair = WordPair.random();
+//     final _suggestions = <WordPair>[];
+//     final _biggerFont = const TextStyle(fontSize: 18);
+//     // return Text(wordPair.asPascalCase);
+//     return ListView.builder(
+//       padding: const EdgeInsets.all(16.0),
+//       itemBuilder: /*1*/ (context, i) {
+//         if (i.isOdd) return const Divider(); /*2*/
 
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      },
-    );
-  }
-}
+//         final index = i ~/ 2; /*3*/
+//         if (index >= _suggestions.length) {
+//           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+//         }
+//         return ListTile(
+//           title: Text(
+//             _suggestions[index].asPascalCase,
+//             style: _biggerFont,
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -48,19 +48,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final wordPair = WordPair.random();
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.arrow_back, 'back'),
+        _buildButtonColumn(color, Icons.arrow_forward, 'forward'),
+        // _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Ở một cái giếng nọ có một con ếch sống lâu năm dưới đáy giếng,'
+        'xung quanh nó chỉ toàn là những con nhái, ốc, cua bé nhỏ.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Welcome to Story',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Welcome to Story'),
         ),
-        body: const Center(
-          child: Text(
-              'Ở một cái giếng nọ có một con ếch sống lâu năm dưới đáy giếng, xung quanh nó chỉ toàn là những con nhái, ốc, cua bé nhỏ. Ở dưới đáy giếng nhìn lên trời, chú ếch chỉ có thể thấy được một khoảng trời rất bé như cái vung vậy.Mỗi lần ếch cất tiếng kêu ồm ộp đều làm các con vật khác trong giếng hoảng sợ nên ếch hênh hoang tự coi mình là chúa tể . Nó đã nghĩ thầm trong đầu rằng: “Tất cả vũ trụ chỉ có như thế, trời bé bằng vung". Vì có suy nghĩ như thế nên nó cứ nhìn lên bầu trời bé xíu ấy và nghĩ nó thì oai phong giống như một vị chúa tể. Ngày nào cũng thấy như vậy nên nó đã khẳng định bầu trời chỉ to bằng cái vung mà thôi. Và rồi một năm trời mưa rất to làm nước trong giếng đầy lên tràn bờ đưa ếch lên miệng giếng. Vẫn quen thói cũ nên ếch câng câng nhìn lên trời. Một điều bất ngờ đập vào mắt ếch chính là nó bỗng thấy cả một bầu trời rộng lớn gấp nhiều lần so với bầu trời bé như vung mà nó vẫn thấy khi ở bên dưới đáy giếng. Ếch không tin vào mắt mình và cảm thấy rất bực bội vì điều đó. Nó đã cất tiếng kêu ồm ộp để ra oai, ếch hi vọng sau những tiếng kêu của mình mọi thứ sẽ phải trở lại như ban đầu. Thế nhưng hiển nhiên là sau tiếng kêu của ếch mọi thứ vẫn vậy, bầu trời to lớn vẫn là bầu trời to lớn. Ếch càng lấy làm lạ và bực bội hơn nữa nên mải nhìn lên bầu trời không thèm để ý đến xung quanh nên nó đã bị một chú trâu đi ngang qua đó dẫm chết.'),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/frog.png',
+              width: 480,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+            // titleSection,
+            textSection,
+            buttonSection,
+          ],
+          // body: const Center(
+          //   child: Text(
+          //       ' Ở dưới đáy giếng nhìn lên trời, chú ếch chỉ có thể thấy được một khoảng trời rất bé như cái vung vậy.Mỗi lần ếch cất tiếng kêu ồm ộp đều làm các con vật khác trong giếng hoảng sợ nên ếch hênh hoang tự coi mình là chúa tể . Nó đã nghĩ thầm trong đầu rằng: “Tất cả vũ trụ chỉ có như thế, trời bé bằng vung". Vì có suy nghĩ như thế nên nó cứ nhìn lên bầu trời bé xíu ấy và nghĩ nó thì oai phong giống như một vị chúa tể. Ngày nào cũng thấy như vậy nên nó đã khẳng định bầu trời chỉ to bằng cái vung mà thôi. Và rồi một năm trời mưa rất to làm nước trong giếng đầy lên tràn bờ đưa ếch lên miệng giếng. Vẫn quen thói cũ nên ếch câng câng nhìn lên trời. Một điều bất ngờ đập vào mắt ếch chính là nó bỗng thấy cả một bầu trời rộng lớn gấp nhiều lần so với bầu trời bé như vung mà nó vẫn thấy khi ở bên dưới đáy giếng. Ếch không tin vào mắt mình và cảm thấy rất bực bội vì điều đó. Nó đã cất tiếng kêu ồm ộp để ra oai, ếch hi vọng sau những tiếng kêu của mình mọi thứ sẽ phải trở lại như ban đầu. Thế nhưng hiển nhiên là sau tiếng kêu của ếch mọi thứ vẫn vậy, bầu trời to lớn vẫn là bầu trời to lớn. Ếch càng lấy làm lạ và bực bội hơn nữa nên mải nhìn lên bầu trời không thèm để ý đến xung quanh nên nó đã bị một chú trâu đi ngang qua đó dẫm chết.'),
           // body: const Center(
           //   child: RandomWords(),
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
