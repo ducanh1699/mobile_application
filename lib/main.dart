27525be2decc06314e29d685049e0a5f1d61d81c
ducanh1699 <ducanh1699@gmail.com>
2022-08-30 12:47:17 +0700

    ducanh sua text + anh

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,25 @@ class MyApp extends StatelessWidget {
       padding: EdgeInsets.all(32),
       child: Text(
         'Ở một cái giếng nọ có một con ếch sống lâu năm dưới đáy giếng,'
-        'xung quanh nó chỉ toàn là những con nhái, ốc, cua bé nhỏ.',
+        'xung quanh nó chỉ toàn là những con nhái, ốc, cua bé nhỏ.'
+        'Ở dưới đáy giếng nhìn lên trời, chú ếch chỉ có thể thấy được một'
+        'khoảng trời rất bé như cái vung vậy. Mỗi lần ếch cất tiếng kêu ồm ộp'
+        'đều làm các con vật khác trong giếng hoảng sợ nên ếch hênh hoang tự coi'
+        'mình là chúa tể . Nó đã nghĩ thầm trong đầu rằng: “Tất cả vũ trụ chỉ '
+        'có như thế, trời bé bằng vung". Vì có suy nghĩ như thế nên nó cứ nhìn'
+        'lên bầu trời bé xíu ấy và nghĩ nó thì oai phong giống như một vị chúa '
+        'tể. Ngày nào cũng thấy như vậy nên nó đã khẳng định bầu trời chỉ to bằng'
+        'cái vung mà thôi. Và rồi một năm trời mưa rất to làm nước trong giếng đầy '
+        'lên tràn bờ đưa ếch lên miệng giếng. Vẫn quen thói cũ nên ếch câng câng'
+        'nhìn lên trời. Một điều bất ngờ đập vào mắt ếch chính là nó bỗng thấy '
+        'cả một bầu trời rộng lớn gấp nhiều lần so với bầu trời bé như vung '
+        'mà nó vẫn thấy khi ở bên dưới đáy giếng. Ếch không tin vào mắt mình'
+        'và cảm thấy rất bực bội vì điều đó. Nó đã cất tiếng kêu ồm ộp để ra oai,'
+        'ếch hi vọng sau những tiếng kêu của mình mọi thứ sẽ phải trở lại như '
+        'ban đầu. Thế nhưng hiển nhiên là sau tiếng kêu của ếch mọi thứ vẫn vậy,'
+        'bầu trời to lớn vẫn là bầu trời to lớn. Ếch càng lấy làm lạ và bực bội'
+        'hơn nữa nên mải nhìn lên bầu trời không thèm để ý đến xung quanh nên nó'
+        'đã bị một chú trâu đi ngang qua đó dẫm chết.',
         softWrap: true,
       ),
     );
@@ -81,18 +99,19 @@ class MyApp extends StatelessWidget {
         body: ListView(
           children: [
             Image.asset(
-              'images/frog.png',
-              width: 480,
-              height: 120,
-              fit: BoxFit.cover,
+              'images/1.jpg',
+              width: 600,
+              height: 400,
+              fit: BoxFit.fill,
+              alignment: Alignment.topCenter,
             ),
             // titleSection,
             textSection,
-            buttonSection,
+            // buttonSection,
           ],
           // body: const Center(
           //   child: Text(
-          //       ' Ở dưới đáy giếng nhìn lên trời, chú ếch chỉ có thể thấy được một khoảng trời rất bé như cái vung vậy.Mỗi lần ếch cất tiếng kêu ồm ộp đều làm các con vật khác trong giếng hoảng sợ nên ếch hênh hoang tự coi mình là chúa tể . Nó đã nghĩ thầm trong đầu rằng: “Tất cả vũ trụ chỉ có như thế, trời bé bằng vung". Vì có suy nghĩ như thế nên nó cứ nhìn lên bầu trời bé xíu ấy và nghĩ nó thì oai phong giống như một vị chúa tể. Ngày nào cũng thấy như vậy nên nó đã khẳng định bầu trời chỉ to bằng cái vung mà thôi. Và rồi một năm trời mưa rất to làm nước trong giếng đầy lên tràn bờ đưa ếch lên miệng giếng. Vẫn quen thói cũ nên ếch câng câng nhìn lên trời. Một điều bất ngờ đập vào mắt ếch chính là nó bỗng thấy cả một bầu trời rộng lớn gấp nhiều lần so với bầu trời bé như vung mà nó vẫn thấy khi ở bên dưới đáy giếng. Ếch không tin vào mắt mình và cảm thấy rất bực bội vì điều đó. Nó đã cất tiếng kêu ồm ộp để ra oai, ếch hi vọng sau những tiếng kêu của mình mọi thứ sẽ phải trở lại như ban đầu. Thế nhưng hiển nhiên là sau tiếng kêu của ếch mọi thứ vẫn vậy, bầu trời to lớn vẫn là bầu trời to lớn. Ếch càng lấy làm lạ và bực bội hơn nữa nên mải nhìn lên bầu trời không thèm để ý đến xung quanh nên nó đã bị một chú trâu đi ngang qua đó dẫm chết.'),
+          //       '                '),
           // body: const Center(
           //   child: RandomWords(),
         ),
